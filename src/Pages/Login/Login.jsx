3cfc1abfd61807a4f9d43efc88e05040b6f8e463
Ronie083 <ahmedronie6@gmail.com>
2023-05-20 01:12:31 +0600

    login & signup authentication done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 
 const Login = () => {
+
+    const { logIn } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log( email, password);
+        logIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.log(error));
+    }
+
     return (
         <div className="container mx-auto rounded-lg hero min-h-screen bg-base-200" style={{ backgroundImage: `url("https://wallpapercave.com/wp/G3XZ1A1.jpg")` }}>
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,21 +31,23 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                        </div>
-                        <div className="form-control mt-6">
-                            <submit className="btn btn-primary">Submit</submit>
-                        </div>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name="password" placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 
 const SignUp = () => {
+
+    const {createUser} = useContext(AuthContext);
+
+    const handleSignUp = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+        createUser(email, password)
+            .then(result =>{
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
         <div>
             <div className="container mx-auto rounded-lg hero min-h-screen bg-base-200" style={{ backgroundImage: `url("https://wallpapercave.com/wp/wp4540008.jpg")` }}>
@@ -19,33 +40,35 @@ const SignUp = () => {
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Your name</span>
-                                </label>
-                                <input type="text" placeholder="name" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Photo URL</span>
-                                </label>
-                                <input type="text" placeholder="photo-url" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                            </div>
-                            <div className="form-control mt-6">
-                                <submit className="btn btn-primary">Submit</submit>
-                            </div>
+                            <form onSubmit={handleSignUp}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Your name</span>
+                                    </label>
+                                    <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" name="email" placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo URL</span>
+                                    </label>
+                                    <input type="text" name="photo" placeholder="photo-url" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="text" name="password" placeholder="password" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className="btn btn-primary" type="submit" value="Sign Up" />
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
